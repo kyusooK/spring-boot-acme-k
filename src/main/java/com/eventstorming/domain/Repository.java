207def336823110f0e557cd1d539505cc843c454
kyusoo <rbtn110@uengine.org>
2023-05-17 14:06:04 +0900

    Repository.java 수정

--- src/main/java/com/eventstorming/domain/Repository.java
+++ src/main/java/com/eventstorming/domain/Repository.java
@@ -13,7 +13,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 //<<< PoEAA / Repository
 @RepositoryRestResource(collectionResourceRel="{{namePlural}}", path="{{namePlural}}")
 public interface {{namePascalCase}}Repository extends PagingAndSortingRepository<{{namePascalCase}}, {{aggregateRoot.keyFieldDescriptor.className}}>{
-  {{#contexts.views.aggregate}}List<{{namePascalCase}}> {{#contexts.views}}{{#queryOption}}{{apiPath}}{{/queryOption}}{{/contexts.views}}({{#contexts.views}}{{#queryParameters}}{{className}} {{nameCamelCase}}{{^@last}}, {{/@last}}{{/queryParameters}}{{/contexts.views}});{{/contexts.views.aggregate}}
+  {{#boundedContext.readModels}}List<{{namePascalCase}}>{{/boundedContext.readModels}} {{#contexts.views}}{{#queryOption}}{{apiPath}}{{/queryOption}}{{/contexts.views}}({{#contexts.views}}{{#queryParameters}}{{className}} {{nameCamelCase}}{{^@last}}, {{/@last}}{{/queryParameters}}{{/contexts.views}});
 }
 //>>> PoEAA / Repository
 <function>
