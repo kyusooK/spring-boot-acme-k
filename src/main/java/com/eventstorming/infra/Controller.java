f7ef33f00d7a4706dbac4fd6ab5f58eac8244d49
kyusoo <rbtn110@uengine.org>
2023-05-17 14:30:58 +0900

    Controller.java 수정

--- src/main/java/com/eventstorming/infra/Controller.java
+++ src/main/java/com/eventstorming/infra/Controller.java
@@ -112,7 +112,7 @@ public class {{ namePascalCase }}Controller {
     {{/each}}
     {{/if}}
     {{#contexts.views}}@PutMapping("/{{#aggregate}}{{namePlural}}{{/aggregate}}/search/{{#queryOption}}{{apiPath}}{{/queryOption}}")
-    public Object {{#queryOption}}{{apiPath}}{{/queryOption}}(@RequestBody {{#aggregate}}{{namePascalCase}}{{/aggregate}}Query query){
+    public Object {{#queryOption}}{{apiPath}}{{/queryOption}}(@RequestBody {{namePascalCase}}Query query){
         return {{#aggregate}}{{nameCamelCase}}{{/aggregate}}Repository.{{#queryOption}}{{apiPath}}{{/queryOption}}({{#queryParameters}}query.get{{namePascalCase}}(){{^@last}},{{/@last}}{{/queryParameters}});{{/contexts.views}}
     }
 }
