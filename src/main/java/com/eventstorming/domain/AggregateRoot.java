27aaba71b4561c6b7eab7b209626bbba99e077a4
kyusoo <rbtn110@uengine.org>
2023-05-19 14:00:30 +0900

    AttributeOverride

--- src/main/java/com/eventstorming/domain/AggregateRoot.java
+++ src/main/java/com/eventstorming/domain/AggregateRoot.java
@@ -257,7 +257,7 @@ window.$HandleBars.registerHelper('checkBigDecimal', function (fieldDescriptors)
 // });
 window.$HandleBars.registerHelper('checkAttribute', function (isOverrideField) {
     if(isOverrideField){
-       {{#if referenceClass}} var overrides = `@AttributeOverride(name="id", column= @Column(name="${nameCamelCase}", nullable=true))\n`; {{/if}}
+        {{#aggregateRoot.fieldDescriptors}}{{#if referenceClass}} var overrides = `@AttributeOverride(name="id", column= @Column(name="{{nameCamelCase}}", nullable=true))\n`; {{/if}} {{/aggregateRoot.fieldDescriptors}}
     }
     return overrides
 });
